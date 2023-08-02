33837d5c8ad18651572ee0876b5d96c21cdffacc
Evocrevo <abresjohnalexis@gmail.com>
2023-08-02 17:44:06 +0800

    fix header icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,20 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      lightGray: "hsl(212, 45%, 89%)",
+      grayishBlue: "hsl(220, 15%, 55%)",
+      darkBlue: "hsl(218, 44%, 22%)",
     },
+    fontFamily: {
+      outfit: ['var(--font-outfit)'],
+    },
+    extend: {
+      width: {
+        128: "32rem"
+      }
+    }
   },
   plugins: [],
 }
